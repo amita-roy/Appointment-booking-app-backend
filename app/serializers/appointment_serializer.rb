a796b855130df7e4c045756a32594ab376a9f0a3
Amita-Roy <sardana.amita2@gmail.com>
2021-04-29 10:38:22 +0200

    Update appointment serailizer

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,5 +1,5 @@
 class AppointmentSerializer < ActiveModel::Serializer
-  attributes :id, :date, :time
+  attributes :id, :date, :time, :total_amount
   belongs_to :user
-  has_many :services
+  has_many :services, through: :SelectedServices
 end
