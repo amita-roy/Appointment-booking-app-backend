7897ae5c112fe16a6ea7486354a3cf12eacf1439
Amita-Roy <sardana.amita2@gmail.com>
2021-05-03 15:03:13 +0200

    Update session route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api do
     namespace :v1 do
-      resource :session, only: [:create]
+      resources :session, only: [:create]
       resources :users, only: %i[index create]
       resources :appointments, only: %i[index create]
       resources :services, only: [:index]
