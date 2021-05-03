0f832962d4491b191ad997cf41a5f5b98a46ad4d
Amita-Roy <sardana.amita2@gmail.com>
2021-05-03 15:32:24 +0200

    Update user model and remove email & password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :appointments, dependent: :delete_all
+  has_many :appointments
+  has_many :services, through: :appointments
 
   validates :name, presence: true, length: { minimum: 3, maximum: 10 }, uniqueness: true
-  validates :email, presence: true, format: { with: /\w+@\w+\.{1}[a-zA-Z]{2,}/ }, uniqueness: true,
-                    length: { minimum: 10, maximum: 30 }
-  validates :password, presence: true, length: { minimum: 6, maximum: 10 }
 end
