12759023286be729f813c32fafafd00818094fea
Amita-Roy <sardana.amita2@gmail.com>
2021-05-03 12:25:32 +0200

    Create a users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::UsersController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: { token: JsonWebToken.encode(sub: user.id), user: user }
+    else
+      render json: { errors: user.errors.full_messages }, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password)
+  end
+end
