1a58897dd92793fafc09e8b7661daeecafab5601
Amita-Roy <sardana.amita2@gmail.com>
2021-05-03 12:40:35 +0200

    Update users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::UsersController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:create]
+
+  def index
+    @users = User.all
+    render json: @user, status: 200
+  end
 
   def create
     user = User.new(user_params)
