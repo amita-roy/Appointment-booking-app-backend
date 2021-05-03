0f4704a213b0f3412646a9d7dd87cbf86dbc21fc
Amita-Roy <sardana.amita2@gmail.com>
2021-05-03 15:17:13 +0200

    Drop appointments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_131116) do
+ActiveRecord::Schema.define(version: 2021_05_03_131600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,16 +43,6 @@ ActiveRecord::Schema.define(version: 2021_05_03_131116) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "appointments", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "date"
-    t.string "time"
-    t.integer "total_amount"
-    t.index ["user_id"], name: "index_appointments_on_user_id"
-  end
-
   create_table "services", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -80,5 +70,4 @@ ActiveRecord::Schema.define(version: 2021_05_03_131116) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "appointments", "users"
 end
