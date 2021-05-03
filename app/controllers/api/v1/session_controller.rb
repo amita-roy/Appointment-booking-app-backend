1e80ad9636e0af9d1ae8bae62dc7b77fd5ca713e
Amita-Roy <sardana.amita2@gmail.com>
2021-05-03 12:25:18 +0200

    Create a session controller

---
+++ app/controllers/api/v1/session_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::SessionController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && (user.valid_password? params[:password])
+      render json: { token: JsonWebToken.encode(sub: user.id), user: user }
+    else
+      render json: { errors: ['Invalid email or password'] }, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password)
+  end
+end
