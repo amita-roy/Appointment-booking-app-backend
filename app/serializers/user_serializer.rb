747b810671299dfa2f57932202057599df4618c1
Amita-Roy <sardana.amita2@gmail.com>
2021-05-04 13:08:02 +0200

    Update user serializer with adding appointments and services

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email
+  attributes :id, :name, :email, :appointments, :services
   has_many :appointments
   has_many :services, through: :appointments
 end
