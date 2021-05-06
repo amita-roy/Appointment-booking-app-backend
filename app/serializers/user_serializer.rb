18f038c9f5f868aac626e4f0f9392d88f3939560
Amita-Roy <sardana.amita2@gmail.com>
2021-05-06 10:10:39 +0200

    Update user serializer with just id, name and email

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :appointments, :services
-  has_many :appointments
-  has_many :services, through: :appointments
+  attributes :id, :name, :email
 end
